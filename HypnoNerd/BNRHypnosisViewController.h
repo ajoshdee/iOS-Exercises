f4077a1150e1a30ab3ff338efcba10cc35407c95
Josh Dee <joshdee@MAC020.local>
2014-10-07 08:45:30 +0800

    HypnoNerd Exercise

---
+++ HypnoNerd/BNRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.h
+//  HypnoNerd
+//
+//  Created by Josh Dee on 9/19/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRHypnosisViewController : UIViewController
+
+@end
