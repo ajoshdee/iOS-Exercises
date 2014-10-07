8ecc961ce7a7a49cabb400ddb2106f0a26e77338
Josh Dee <joshdee@MAC020.local>
2014-10-07 08:49:16 +0800

    Hypnosister Exercise

---
+++ Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Josh Dee on 9/18/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
