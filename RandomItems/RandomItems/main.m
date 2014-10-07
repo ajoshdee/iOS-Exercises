ab3e99232fda05deb6ea919c8c4b49b9040036ec
Josh Dee <joshdee@MAC020.local>
2014-10-07 08:52:11 +0800

    RandomItems Exercise

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,42 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Josh Dee on 9/18/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BNRItem.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        /*for (int i = 0; i < 10; i++) {
+            BNRItem *item = [BNRItem randomItem];
+            [items addObject:item];
+        }*/
+        BNRItem *backpack = [[BNRItem alloc] initWithItemName:@"Backpack"];
+        [items addObject:backpack];
+        BNRItem *calculator = [[BNRItem alloc] initWithItemName:@"Calculator"];
+        [items addObject:calculator];
+        backpack.containedItem = calculator;
+        backpack = nil;
+        calculator = nil;
+        
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+        NSLog(@"Setting items to nil...");
+        // Destroy the mutable array object
+        items = nil;
+        
+    }
+    return 0;
+}
+
