9749f3741601516075ab5588b3df8d8f26bdc89a
Josh Dee <joshdee@MAC020.local>
2014-10-07 15:40:47 +0800

    Quiz exercise

---
+++ Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Josh Dee on 9/18/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ Quiz/Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Josh Dee on 9/18/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
