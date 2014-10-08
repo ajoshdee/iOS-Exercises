bb0ab4d52df1a8a39c6aa9eafb8763a7ffc2c515
Josh Dee <joshdee@MAC020.local>
2014-10-08 15:29:24 +0800

    add UIImage animation

---
+++ SimpleAnimation/SimpleAnimation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleAnimation
+//
+//  Created by Josh Dee on 10/8/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
