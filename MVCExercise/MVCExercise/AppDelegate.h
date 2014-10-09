d430a24cf740c7d6f545afd7f6c7472f5b095bdb
Josh Dee <joshdee@MAC020.local>
2014-10-09 10:30:38 +0800

    new project MVCExercise

---
+++ MVCExercise/MVCExercise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MVCExercise
+//
+//  Created by Josh Dee on 10/9/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
