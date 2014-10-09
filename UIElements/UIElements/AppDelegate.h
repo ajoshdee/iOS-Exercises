8f4f034b215fe117f0d2a68654c44d7210e71884
Josh Dee <joshdee@MAC020.local>
2014-10-09 16:43:18 +0800

    new project UIElements

---
+++ UIElements/UIElements/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIElements
+//
+//  Created by Josh Dee on 10/9/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
