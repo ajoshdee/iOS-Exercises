b1c7f8c5ee411b510dbaa8ab1d25bdc92c5c1ae7
Josh Dee <joshdee@MAC020.local>
2014-10-10 09:23:13 +0800

    add tab View Controller

---
+++ UIElements/UIElements/TabViewController.m
@@ -0,0 +1,38 @@
+//
+//  TabViewController.m
+//  UIElements
+//
+//  Created by Josh Dee on 10/10/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import "TabViewController.h"
+
+@interface TabViewController ()
+
+@end
+
+@implementation TabViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
