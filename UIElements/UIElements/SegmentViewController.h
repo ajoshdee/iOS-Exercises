b885e544d73cbe716361f3a4712a07716c675aa9
Josh Dee <joshdee@MAC020.local>
2014-10-10 09:19:06 +0800

    add segmented view control

---
+++ UIElements/UIElements/SegmentViewController.h
@@ -0,0 +1,13 @@
+//
+//  SegmentViewController.h
+//  UIElements
+//
+//  Created by Josh Dee on 10/10/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SegmentViewController : UIViewController
+
+@end
---
+++ UIElements/UIElements/SegmentViewController.m
@@ -0,0 +1,38 @@
+//
+//  SegmentViewController.m
+//  UIElements
+//
+//  Created by Josh Dee on 10/10/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import "SegmentViewController.h"
+
+@interface SegmentViewController ()
+
+@end
+
+@implementation SegmentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
