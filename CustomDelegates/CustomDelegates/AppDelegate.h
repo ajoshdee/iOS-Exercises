5391bfa804c72b2107ecf8996378f8f527a4e345
Josh Dee <joshdee@MAC020.local>
2014-10-13 14:06:59 +0800

    New project CustomDelegates

---
+++ CustomDelegates/CustomDelegates/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomDelegates
+//
+//  Created by Josh Dee on 10/13/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
